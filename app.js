1a535e4cc7dcc1495a8201771d51a5e1fa98469a
Jtom91 <jtom29544@gmail.com>
2022-02-05 09:05:02 +0100

    hello

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const nodemailer = require('nodemailer')
 const path = require('path')
-// const cors = require('cors')
+const cors = require('cors')
 require('dotenv').config()
 
 
